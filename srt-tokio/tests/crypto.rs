07225f86efbc6e095d9de6d28a459c6b57c599a7
Russell Greene <russell@shotover.com>
2023-03-23 18:09:47 -0500

    don't use co;nflicting addresses

--- srt-tokio/tests/crypto.rs
+++ srt-tokio/tests/crypto.rs
@@ -3,7 +3,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use srt_tokio::SrtSocket;
+use srt_tokio::{SrtSocket};
 
 use assert_matches::assert_matches;
 use bytes::Bytes;
@@ -11,14 +11,16 @@ use futures::{SinkExt, TryStreamExt};
 use log::info;
 use tokio::{select, spawn, time::sleep};
 
-async fn test_crypto(size_listen: u16, size_call: u16) {
+async fn test_crypto(size_listen: u16, size_call: u16, port: u16) {
     let sender = SrtSocket::builder()
         .encryption(size_listen, "password123")
-        .listen_on(":2000");
+        .listen_on(port);
+
+    let local_addr = format!("127.0.0.1:{port}");
 
     let recvr = SrtSocket::builder()
         .encryption(size_call, "password123")
-        .call("127.0.0.1:2000", None);
+        .call(local_addr.as_str(), None);
 
     let t = spawn(async move {
         let mut sender = sender.await.unwrap();
@@ -45,20 +47,16 @@ async fn test_crypto(size_listen: u16, size_call: u16) {
 async fn crypto_exchange() {
     let _ = pretty_env_logger::try_init();
 
-    test_crypto(16, 16).await;
-    sleep(Duration::from_millis(100)).await;
-    test_crypto(24, 24).await;
-    sleep(Duration::from_millis(100)).await;
-    test_crypto(32, 32).await;
+    test_crypto(16, 16, 2000).await;
+    test_crypto(24, 24, 2001).await;
+    test_crypto(32, 32, 2002).await;
 }
 
 #[tokio::test]
 async fn key_size_mismatch() {
-    test_crypto(32, 16).await;
-    sleep(Duration::from_millis(100)).await;
-    test_crypto(32, 0).await;
-    sleep(Duration::from_millis(100)).await;
-    test_crypto(0, 32).await;
+    test_crypto(32, 16, 2003).await;
+    test_crypto(32, 0, 2004).await;
+    test_crypto(0, 32, 2005).await;
 }
 
 #[tokio::test]
